891c72cb7c70825635ab59549810a7674674fd52
SayemKaderChowdhury <kaderchowdhury.sayem@gmail.com>
2023-11-04 01:07:09 +0600

    added comment about urls

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+# The URL declarations for this Django project;
+# a “table of contents” of your Django-powered site
 urlpatterns = [
     path("", views.index, name="index")
 ]
