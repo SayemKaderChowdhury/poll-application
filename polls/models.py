e695ac5b431b080a0b491a13500d98179a4b43ee
SayemKaderChowdhury <kaderchowdhury.sayem@gmail.com>
2023-11-04 21:35:28 +0600

    New method of timezone added in Question model

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 # A Question has a question and a publication date.
@@ -7,6 +10,9 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 # A Choice has two fields: the text of the choice and a vote tally. Each Choice is associated with a Question.
 class Choice(models.Model):
