c2c9c478986325f49fbfd0cc93c0c3443daffeda
SayemKaderChowdhury <kaderchowdhury.sayem@gmail.com>
2023-11-05 23:50:41 +0600

    Namespace added in the main url directory

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
+app_name = "polls"
 # The URL declarations for this Django project;
 # a “table of contents” of your Django-powered site
 urlpatterns = [
